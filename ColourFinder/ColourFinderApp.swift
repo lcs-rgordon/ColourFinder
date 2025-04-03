6eeefe737217cffbb2c12a9748e501a2c989da0c
Russell Gordon <rgordon@lcs.on.ca>
2025-04-03 12:06:10 -0400

    Initial Commit

---
+++ ColourFinder/ColourFinderApp.swift
@@ -0,0 +1,17 @@
+//
+//  ColourFinderApp.swift
+//  ColourFinder
+//
+//  Created by Russell Gordon on 2025-04-03.
+//
+
+import SwiftUI
+
+@main
+struct ColourFinderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ColourFinder/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ColourFinder
+//
+//  Created by Russell Gordon on 2025-04-03.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
